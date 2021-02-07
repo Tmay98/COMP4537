7d67243918557df6855284e03d93e124b8ddafc3
Tommy May <46503072+twmay98@users.noreply.github.com>
2021-02-06 23:55:35 -0800

    lab 4

--- COMP4537/labs/4/writeFile.js
+++ COMP4537/labs/4/writeFile.js
@@ -5,10 +5,16 @@ var fs = require('fs');
 http.createServer(function (req, res) {
     const q = url.parse(req.url, true);
     const filename = "." + q.pathname;
-    fs.appendFile('file.txt',  q.query["text"], function (err) {
-        if (err) console.log('error!');
-        console.log('Updated!');
-    });
+    let text = q.query["text"];
+    if (text) {
+        fs.appendFile('file.txt', q.query["text"], function (err) {
+            if (err) return res.end('Error please enter text');
+            return res.end('file.txt updated');
+        });
+    }
+    else{
+        return res.end("Error enter text please")
+    }
 }
 ).listen(process.env.PORT || 3000);
 console.log('listening ...');
